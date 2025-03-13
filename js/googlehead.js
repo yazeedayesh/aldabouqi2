a402b32c5139b6a61b492c96469ebcd65fef6092
Yazeed ayesh <yazeedayesh97@hotmail.com>
2025-03-13 13:57:21 +0300

    Update googlehead.js

--- js/googlehead.js
+++ js/googlehead.js
@@ -1,24 +1,32 @@
-<!-- ✅ Google Tag Manager (Head) -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10794969963"></script>
 <script>
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:''; j.async=true;
-j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K35CB7ML');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10794969963');
 </script>
-<!-- ✅ End Google Tag Manager -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9FH7PMRW14"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-<!-- ✅ Google Analytics (دمج الحسابين في gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9FH7PMRW14"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-// ✅ ضبط التتبع لجميع الحسابات في نفس السكريبت
-gtag('config', 'G-9FH7PMRW14');
-gtag('config', 'AW-10794969963');
-</script>
-
-<!-- ✅ Google Ads (محسّن بدون تأثير على الأداء) -->
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" 
-    data-ad-client="ca-pub-4827066865150686" crossorigin="anonymous"></script>
+        gtag('config', 'G-9FH7PMRW14');
+    </script>
+    
+      <!-- Google ads -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4827066865150686"
+     crossorigin="anonymous">
+        
+     </script>
+    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K35CB7ML');</script>
+<!-- End Google Tag Manager -->
